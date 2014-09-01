698f87de45fa91e85b67f698e8c4dcb840a27c00
Marco <marco@primaldev.nl>
2014-09-01 15:56:17 +0200

    Added Process start form

--- PackageProcessManager/src/org/primaldev/ppm/event/SwitchView_Event.java
+++ PackageProcessManager/src/org/primaldev/ppm/event/SwitchView_Event.java
@@ -1,15 +1,27 @@
 package org.primaldev.ppm.event;
 
+import org.activiti.engine.repository.ProcessDefinition;
+
 import com.github.wolfie.blackboard.Event;
 
 public class SwitchView_Event implements Event{
 	private String viewName = null;
-
+	private ProcessDefinition processDefinition = null;
+	
 	public SwitchView_Event(String s) {
 		viewName = s;
+	}
+	
+	public SwitchView_Event(String s, ProcessDefinition p) {
+		viewName = s;
+		processDefinition=p;
 	}
 
 	public String getViewName() {
 		return viewName;
+	}
+	
+	public ProcessDefinition getProcessDefinition() {
+		return processDefinition;
 	}
 }
